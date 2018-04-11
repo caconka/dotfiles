f3731c10c66a9729b3c572aa45d07e155046d382
Carlos Cabezas <carloscabezascontra@gmail.com>
2018-04-11 15:31:45 +0200

    hibernate

--- scripts/my-xfce.sh
+++ scripts/my-xfce.sh
@@ -3,11 +3,13 @@
 # XFCE Configs
 
 sudo pacman -Syy --noconfirm gnome-screensaver deepin-icon-theme arc-gtk-theme \
-  dockbarx vala-panel-appmenu-xfce-git blueberry
+  blueberry
+
+pacaur -Syy --noconfirm dockbarx vala-panel-appmenu-xfce-git
 
 mkdir ~/.icons ~/.themes
 rm -rf ~/.config/xfce4/xfconf/xfce-perchannel-xml
 
 ln -s --force ~/.dotfiles/xfce-configs ~/.config/xfce4/xfconf/xfce-perchannel-xml
-ln -s /usr/share/icons/deepin ~/.icons/
+cp -r /usr/share/icons/deepin ~/.icons/
 
