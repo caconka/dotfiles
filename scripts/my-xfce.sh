f4fad54122597b4f235706b4609282ce421bb89e
Carlos Cabezas <carloscabezascontra@gmail.com>
2018-06-04 20:11:25 +0200

    remove dockbarx xfce, add youcompleteme to vimrc

--- scripts/my-xfce.sh
+++ scripts/my-xfce.sh
@@ -5,8 +5,6 @@
 sudo pacman -Syy --noconfirm gnome-screensaver deepin-icon-theme arc-gtk-theme \
   blueberry
 
-pacaur -Syy --noconfirm dockbarx vala-panel-appmenu-xfce-git
-
 mkdir ~/.icons ~/.themes
 rm -rf ~/.config/xfce4/xfconf/xfce-perchannel-xml
 
