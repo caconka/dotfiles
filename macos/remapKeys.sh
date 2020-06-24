1ebeb2c05f1625d6aaed8891a84266a5e4ffe78c
Carlos Cabezas <carloscabezascontra@gmail.com>
2020-06-24 11:58:26 +0200

    macos remap Keys

---
+++ macos/remapKeys.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Documentación para configurar las teclas del teclado (los códigos son los de la tabla e4 Right Ctrl e2 Left Alt)
+# https://developer.apple.com/library/archive/technotes/tn2450/_index.html
+
+hidutil property --set '{"UserKeyMapping":
+    [{"HIDKeyboardModifierMappingSrc":0x7000000e4,
+      "HIDKeyboardModifierMappingDst":0x7000000e2}]
+}'
