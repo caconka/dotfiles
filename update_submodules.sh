38e2a1e1ce4dec9e70e8a7b80883ecb196dfbd94
Carlos Cabezas <carloscabezascontra@gmail.com>
2022-11-30 21:36:11 +0100

    update submodules

--- update_submodules.sh
+++ update_submodules.sh
@@ -3,14 +3,14 @@
 printf "\033[0;32mUpdating submodules\033[0m\n"
 
 printf "\033[0;32mCheckout to master\033[0m\n"
-git submodule foreach git ch master
+git submodule foreach git switch master
 
 printf "\033[0;32mUpdating master branches\033[0m\n"
 git submodule foreach git pull origin master
 
 printf "\033[0;32mUpdating release branch coc's module\033[0m\n"
 cd vim/pack/caconka/start/coc
-git ch release
+git switch release
 git pull origin release
 
 printf "\033[0;32mCommit && push\033[0m\n"
