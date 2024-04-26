70903e35bbd8b32601de4c9ff882a5fba90921e8
caconka <28691030+caconka@users.noreply.github.com>
2024-04-26 16:37:00 +0200

    theme config

--- nvim/lua/caconka/config/theme.lua
+++ nvim/lua/caconka/config/theme.lua
@@ -4,5 +4,5 @@ vim.opt.background = "light"
 vim.cmd("hi TreesitterContext guibg=white")
 
 vim.api.nvim_command("au BufEnter * if &diff | colorscheme catppuccin | endif")
-vim.api.nvim_command("au BufHidden *.{md,json,yaml,yml} colorscheme github_light_default")
+vim.api.nvim_command("au BufHidden *.{md,json,yaml,yml} if &bg == 'light' | colorscheme github_light_default | endif")
 vim.api.nvim_command("au BufEnter *.{md,json,yaml,yml} colorscheme catppuccin")
--- nvim/lua/caconka/plugins/vim-pencil.lua
+++ nvim/lua/caconka/plugins/vim-pencil.lua
@@ -5,18 +5,18 @@ return {
 
 		vim.g["pencil#textwidth"] = 90
 		vim.g["pencil#conceallevel"] = 2
-		-- vim.g["pencil#concealcursor"] = "n"
-		--
-		-- vim.api.nvim_create_autocmd("BufEnter", {
-		-- 	pattern = { "*.md", "*.txt", "*.norg" },
-		-- 	command = "PencilHard | set textwidth=90 | set colorcolumn=0",
-		-- 	group = group
-		-- })
-		--
-		-- vim.api.nvim_create_autocmd("BufLeave", {
-		-- 	pattern = { "*.md", "*.txt", "*.norg" },
-		-- 	command = "PencilOff | set textwidth=0 | set colorcolumn=+1",
-		-- 	group = group
-		-- })
+		vim.g["pencil#concealcursor"] = "n"
+
+		vim.api.nvim_create_autocmd("BufEnter", {
+			pattern = { "*.md", "*.txt", "*.norg" },
+			command = "PencilHard | set textwidth=90 | set colorcolumn=0",
+			group = group
+		})
+
+		vim.api.nvim_create_autocmd("BufLeave", {
+			pattern = { "*.md", "*.txt", "*.norg" },
+			command = "PencilOff | set textwidth=0 | set colorcolumn=+1",
+			group = group
+		})
 	end,
 }
