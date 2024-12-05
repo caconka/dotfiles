b07f0f23f33fc64b16071e077f757d9c5f1545b8
caconka <28691030+caconka@users.noreply.github.com>
2024-12-05 15:15:25 +0100

    update tmux and wezterm

--- shell/wezterm/wezterm.lua
+++ shell/wezterm/wezterm.lua
@@ -8,6 +8,13 @@ config.font = wezterm.font('SF Mono', { weight = 'Medium' })
 config.font_size = 10.25
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- disable ligatures
 
+config.window_padding = {
+	left = '0.33cell',
+	right = 0,
+	top = '0.2cell',
+	bottom = 0,
+}
+
 config.color_scheme = 'Catppuccin Latte'
 config.colors = {
 	background = 'white',
