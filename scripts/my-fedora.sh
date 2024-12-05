630e2fec0014f14e968cb47260e5506a28920f81
caconka <28691030+caconka@users.noreply.github.com>
2024-12-05 15:37:41 +0100

    update fedora script

--- scripts/my-fedora.sh
+++ scripts/my-fedora.sh
@@ -96,25 +96,16 @@ sudo ln -s ~/.dotfiles/idea/idea.conf /etc/sysctl.d/
 
 # Gcloud & kubectl
 sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
-[google-cloud-sdk]
-name=Google Cloud SDK
-baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el9-x86_64
 enabled=1
 gpgcheck=1
 repo_gpgcheck=0
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOM
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-sudo dnf install -y google-cloud-sdk kubectl
+sudo dnf install -y google-cloud-sdk
+gcloud components install gke-gcloud-auth-plugin kubectl
 
 # Docker
 sudo dnf install -y docker-cli containerd
@@ -179,6 +170,11 @@ nvm use node
 
 # nvidia drivers
 #sudo dnf install akmod-nvidia
+
+# Brave
+sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install -y brave-browser
 
 # multimedia codecs
 sudo dnf groupupdate multimedia sound-and-video
