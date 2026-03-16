ddae64610ecd52bb988eeba949f462cd2101e3e9
caconka <28691030+caconka@users.noreply.github.com>
2026-03-16 10:08:00 +0100

    update nvim configs

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,4 @@
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.opt_local.colorcolumn = "0"
+vim.opt_local.conceallevel = 0
--- nvim/lua/caconka/plugins/vim-pencil.lua
+++ nvim/lua/caconka/plugins/vim-pencil.lua
@@ -5,19 +5,19 @@ return {
 		local group = vim.api.nvim_create_augroup("pencil", {clear = true})
 
 		vim.g["pencil#textwidth"] = 90
-		vim.g["pencil#conceallevel"] = 2
+		vim.g["pencil#conceallevel"] = 0
 		vim.g["pencil#concealcursor"] = "n"
 
-		-- vim.api.nvim_create_autocmd("BufEnter", {
-		-- 	pattern = { "*.md" },
-		-- 	command = "PencilHard | set textwidth=90",
-		-- 	group = group
-		-- })
-		--
-		-- vim.api.nvim_create_autocmd("BufLeave", {
-		-- 	pattern = { "*.md" },
-		-- 	command = "PencilOff | set textwidth=0",
-		-- 	group = group
-		-- })
+		vim.api.nvim_create_autocmd("BufEnter", {
+			pattern = { "*.md" },
+			command = "PencilHard | set textwidth=90",
+			group = group
+		})
+
+		vim.api.nvim_create_autocmd("BufLeave", {
+			pattern = { "*.md" },
+			command = "PencilOff | set textwidth=0",
+			group = group
+		})
 	end,
 }
